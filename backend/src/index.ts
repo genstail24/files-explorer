6f77a8e9178c2bda988ac95afa1d09f5ef60a582
genstail24 <45913927+genstail24@users.noreply.github.com>
2025-01-05 00:09:32 +0700

    Initial Commit

---
+++ backend/src/index.ts
@@ -0,0 +1,14 @@
+import { swagger } from "@elysiajs/swagger";
+import { Elysia } from "elysia";
+
+const app = new Elysia()
+
+app.use(swagger());
+app.get("/", () => "Hello Elysia")
+
+app
+  .listen(3000);
+
+console.log(
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+);
