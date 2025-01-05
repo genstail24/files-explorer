97b0bc29992d0ce5a92cb122abe10f181a267824
genstail24 <45913927+genstail24@users.noreply.github.com>
2025-01-05 17:59:15 +0700

    init frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="h-screen w-screen flex justify-center items-center">
+    <h1 class="text-red-900 font-semibold text-xl">Hello World!</h1>
+  </div>
+</template>
+
+<style scoped>
+</style>
